7c9cc90c46636eb76d92aef25b13140ebf6c3e42
Julio Makdisse Saito <saitodisse@gmail.com>
2015-07-22 16:07:08 -0300

    enabling hot load

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+var Webpack = require('webpack');
 var path = require('path');
 var isProduction = process.env.NODE_ENV === 'production';
 var node_modules = path.resolve(__dirname, 'node_modules');
 
 var config = {
-  entry: path.resolve(__dirname, 'src/main.js'),
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+    path.resolve(__dirname, 'src/main.js')
+  ],
   devtool: 'eval-source-map',
   output: {
     path: path.resolve(__dirname, 'src'),
@@ -18,7 +23,8 @@ var config = {
       loader: 'babel?optional=es7.decorators',
       exclude: node_modules
     }]
-  }
+  },
+  plugins: [new Webpack.HotModuleReplacementPlugin()]
 };
 
 module.exports = config;
