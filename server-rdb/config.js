4c8fb6f24e027dcbe71999e43da2f97a6ecf660d
Julio Makdisse Saito <saitodisse@gmail.com>
2015-07-23 00:01:57 -0300

    rdb server working

---
+++ server-rdb/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rethinkdb: {
+        host: 'rethinkdb.dev.azk.io',
+        port: 28015,
+        authKey: '',
+        db: 'tasks_cerebral'
+    },
+    express: {
+        port: 3000
+    }
+};
--- server-rdb/server.js
+++ server-rdb/server.js
@@ -1,10 +1,11 @@
 // Import express and co
+var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
 // Load config for RethinkDB and express
-var config = require(__dirname+"/config.js")
+var config = require(path.join(__dirname, '/config.js'));
 
 // Import rethinkdbdash
 //var thinky = require('thinky')(config.rethinkdb);
@@ -13,7 +14,7 @@ var r = thinky.r;
 var type = thinky.type;
 
 // Create the model
-var Todo = thinky.createModel("todos", {
+var Task = thinky.createModel('tasks', {
     id: type.string(),
     title: type.string(),
     completed: type.boolean(),
@@ -21,65 +22,65 @@ var Todo = thinky.createModel("todos", {
 });
 
 // Ensure that an index createdAt exists
-Todo.ensureIndex("createdAt");
+Task.ensureIndex('createdAt');
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser());
 
-app.route('/todo/get').get(get);
-app.route('/todo/new').put(create);
-app.route('/todo/update').post(update);
-app.route('/todo/delete').post(del);
+app.route('/task/get').get(get);
+app.route('/task/new').put(create);
+app.route('/task/update').post(update);
+app.route('/task/delete').post(del);
 
-
-// Retrieve all todos
-function get(req, res, next) {
-    Todo.orderBy({index: "createdAt"}).run().then(function(result) {
-        res.send(JSON.stringify(result));
-    }).error(handleError(res));
-}
-
-// Create a new todo
-function create(req, res, next) {
-    var todo = new Todo(req.body);
-    todo.save().then(function(result) {
-        res.send(JSON.stringify(result));
-    }).error(handleError(res));
-}
-
-// Update a todo
-function update(req, res, next) {
-    var todo = new Todo(req.body);
-    Todo.get(todo.id).update({
-       title: req.body.title,
-       completed: req.body.completed
-    }).run().then(function(todo) {
-        res.send(JSON.stringify(todo));
-    }).error(handleError(res));
-
-    // Another way to delete a todo is with
-    // Todo.get(req.body.id).update(todo).execute()
-}
-
-// Delete a todo
-function del(req, res, next) {
-    Todo.get(req.body.id).run().then(function(todo) {
-        todo.delete().then(function(result) {
-            res.send("");
-        }).error(handleError(res));
-    }).error(handleError(res));
-
-    // Another way to delete a todo is with
-    // Todo.get(req.body.id).delete().execute()
-}
 
 function handleError(res) {
     return function(error) {
         return res.send(500, {error: error.message});
-    }
+    };
+}
+
+// Retrieve all tasks
+function get(req, res, next) {
+    Task.orderBy({index: 'createdAt'}).run().then(function(result) {
+        res.send(JSON.stringify(result));
+    }).error(handleError(res));
+}
+
+// Create a new task
+function create(req, res, next) {
+    var task = new Task(req.body);
+    task.save().then(function(result) {
+        res.send(JSON.stringify(result));
+    }).error(handleError(res));
+}
+
+// Update a task
+function update(req, res, next) {
+    var task = new Task(req.body);
+    Task.get(task.id).update({
+       title: req.body.title,
+       completed: req.body.completed
+    }).run().then(function(task_response) {
+        res.send(JSON.stringify(task_response));
+    }).error(handleError(res));
+
+    // Another way to delete a task is with
+    // Task.get(req.body.id).update(task).execute()
+}
+
+// Delete a task
+function del(req, res, next) {
+    Task.get(req.body.id).run().then(function(task) {
+        task.delete().then(function(result) {
+            res.send('');
+        }).error(handleError(res));
+    }).error(handleError(res));
+
+    // Another way to delete a task is with
+    // Task.get(req.body.id).delete().execute()
 }
 
 // Start express
-app.listen(config.express.port);
-console.log('listening on port '+config.express.port);
+app.listen(config.express.port, '0.0.0.0');
+console.log('listening on 0.0.0.0:' + config.express.port);
