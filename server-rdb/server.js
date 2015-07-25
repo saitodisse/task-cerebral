410e865f2a8f8060e03d5ccdb8f06462e1bd9411
Julio Makdisse Saito <saitodisse@gmail.com>
2015-07-25 17:11:49 -0300

    save and delete

--- server-rdb/server.js
+++ server-rdb/server.js
@@ -4,11 +4,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-// save envs to etcd
-var saveEtcd = require('./save-etcd');
-saveEtcd('RETHINKDB_HTTP_HOST', process.env.RETHINKDB_HTTP_HOST);
-saveEtcd('NODE_ENV', process.env.NODE_ENV);
-
 // Load config for RethinkDB and express
 var config = require(path.join(__dirname, '/config.js'));
 
@@ -19,70 +14,76 @@ var type = thinky.type;
 
 // Create the model
 var Task = thinky.createModel('tasks', {
-    id: type.string(),
-    title: type.string(),
-    completed: type.boolean(),
-    createdAt: type.date().default(r.now())
+  id: type.string(),
+  title: type.string(),
+  createdAt: type.date().default(r.now())
 });
 
 // Ensure that an index createdAt exists
 Task.ensureIndex('createdAt');
 
 
-app.use(express.static(path.join(__dirname, '/public')));
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+}
+
+// app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser());
+app.use(allowCrossDomain);
 
 app.route('/task/get').get(get);
 app.route('/task/new').put(create);
 app.route('/task/update').post(update);
 app.route('/task/delete').post(del);
 
-
 function handleError(res) {
-    return function(error) {
-        return res.send(500, {error: error.message});
-    };
+  return function(error) {
+    return res.send(500, {error: error.message});
+  };
 }
 
 // Retrieve all tasks
 function get(req, res, next) {
-    Task.orderBy({index: 'createdAt'}).run().then(function(result) {
-        res.send(JSON.stringify(result));
-    }).error(handleError(res));
+  Task.orderBy({index: 'createdAt'}).run().then(function(result) {
+    res.send(JSON.stringify(result));
+  }).error(handleError(res));
 }
 
 // Create a new task
 function create(req, res, next) {
-    var task = new Task(req.body);
-    task.save().then(function(result) {
-        res.send(JSON.stringify(result));
-    }).error(handleError(res));
+  var task = new Task(req.body);
+  task.save().then(function(result) {
+    res.send(JSON.stringify(result));
+  }).error(handleError(res));
 }
 
 // Update a task
 function update(req, res, next) {
-    var task = new Task(req.body);
-    Task.get(task.id).update({
-       title: req.body.title,
-       completed: req.body.completed
-    }).run().then(function(task_response) {
-        res.send(JSON.stringify(task_response));
-    }).error(handleError(res));
+  var task = new Task(req.body);
+  Task.get(task.id).update({
+   title: req.body.title
+  }).run().then(function(task_response) {
+   res.send(JSON.stringify(task_response));
+  }).error(handleError(res));
 
-    // Another way to delete a task is with
-    // Task.get(req.body.id).update(task).execute()
+  // Another way to delete a task is with
+  // Task.get(req.body.id).update(task).execute()
 }
 
 // Delete a task
 function del(req, res, next) {
-    Task.get(req.body.id).run().then(function(task) {
-        task.delete().then(function(result) {
-            res.send('');
-        }).error(handleError(res));
+  Task.get(req.body.id).run().then(function(task) {
+    task.delete().then(function(result) {
+      res.send(req.body.id + ' was deleted');
     }).error(handleError(res));
+  }).error(handleError(res));
 
-    // Another way to delete a task is with
-    // Task.get(req.body.id).delete().execute()
+  // Another way to delete a task is with
+  // Task.get(req.body.id).delete().execute()
 }
 
 // Start express
